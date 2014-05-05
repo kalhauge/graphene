e1e064a974826804e1a74ace86c289171aab077c
Christian Gram Kalhauge <christian@kalhauge.dk>
2014-05-05 11:12:17 -0700

    Removed unesseary contexts

--- examples/triangle.py
+++ examples/triangle.py
@@ -8,18 +8,12 @@ The purpose of this example is to draw a simple triangle.
 
 from graphene import sdl
 from graphene import gl
-from graphene.sdl import gl as sdl_gl
+
 SIZE = (600, 600)
+
 
 def main():
     sdl.init({'video'})
-
-    sdl_gl.set_attribute(
-        context_major_version=3,
-        context_minor_version=2,
-        doublebuffer=1,
-        depth_size=24
-    )
 
     window = sdl.Window.create(
         title='Hello World',
@@ -28,18 +22,15 @@ def main():
         flags={'opengl', 'shown'}
     )
     
-    glcontext = sdl_gl.Context.create(window)
-    print(glcontext._handle)
-#    renderer = sdl.Renderer.create(
-#        window=window,
-#        index=-1,
-#        flags={'accelerated', 'targettexture'}
-#    )
+    renderer = sdl.Renderer.create(
+        window=window,
+        index=-1,
+        flags={'accelerated', 'targettexture'}
+    )
 
-#   print(renderer.info())
-    
+    print(renderer.info())
     print(gl.context_info())
-
+    
     running = True
     while running:
         for event in sdl.event.pending_events():
@@ -48,9 +39,8 @@ def main():
                 running = False
                 break;
     
-    glcontext.delete()
     window.destroy()
-#    renderer.destroy()
+    renderer.destroy()
     sdl.quit() 
    
 if __name__ == '__main__':
